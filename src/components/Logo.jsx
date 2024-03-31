1bd96c88cc97a30e499a1df8379b053f5134444e
JUAN <erickuniv@gmail.com>
2024-03-31 23:19:12 +0700

    no message

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from "../assets/images/logo.png"
 export const Logo = () => {
   return (
-    <div className=' my-6 mx-6 xl:w-[450px] xl:mx-10  lg:w-[400px] md:w-[350px]  sm:w-[300px]  w-[250px]'>
+    <div className=' my-6 mx-6 xl:w-[450px] xl:mx-10  lg:w-[350px] md:w-[350px]  sm:w-[300px]  w-[250px]'>
       <img src={logo} alt="" />
     </div>
   )
